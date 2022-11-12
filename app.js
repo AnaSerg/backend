3bfa3287d99e6576341e72732206d8654c684040
Anastasia Sergeeva <lirebook@yandex.ru>
2022-11-12 23:27:50 +0300

    fix: исправлены ошибки

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
