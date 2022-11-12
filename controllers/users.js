9eef4c7f5dc85d34588fe897ae38d9d755395595
Anastasia Sergeeva <lirebook@yandex.ru>
2022-11-13 00:14:43 +0300

    fix: исправить обработку ошибок

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserById = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
         res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
