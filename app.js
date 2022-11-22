8402356d7be85045395e186cae6d38f422494dfc
Anastasia Sergeeva <lirebook@yandex.ru>
2022-11-23 01:31:22 +0300

    feat: добавить email, password, добработать createUser

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { NOT_FOUND } = require('./errors/errorStatus');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
--- models/user.js
+++ models/user.js
@@ -1,19 +1,36 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    default: 'Жак-Ив Кусто',
     required: true,
     minLength: 2,
     maxLength: 30,
   },
   about: {
     type: String,
+    default: 'Исследователь',
     required: true,
     minLength: 2,
     maxLength: 30,
   },
   avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png';
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: 'Неверный формат email',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
