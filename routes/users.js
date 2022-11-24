b8f04a675fba051bf4d7f7ed1eb0b73d6dc5b883
Anastasia Sergeeva <lirebook@yandex.ru>
2022-11-25 01:10:34 +0300

    fix: исправить ошибки

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ const userRouter = Router();
 userRouter.get('/', getUsers);
 userRouter.get('/:userId', celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().alphanum().length(24).hex(),
+    userId: Joi.string().alphanum().length(24).hex(),
   }),
 }), getUserById);
 userRouter.get('/me', getCurrentUser);
