6c52bbce9af5a2aaca5252b00c76fc94e114c74b
Anastasia Sergeeva <91882066+AnaSerg@users.noreply.github.com>
2022-12-06 01:37:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
